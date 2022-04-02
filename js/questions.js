964e49d4c038e16c79b86130d2a3557419d31194
Tarun Gaur <70098839+TG922@users.noreply.github.com>
2022-04-02 12:26:27 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,139 @@
+// creating an array and passing the number, questions, options, and answers
+//Created By https://github.com/TG922
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Who is making the Web standards?",
+    answer: "WWW",
+    options: [
+      "WWW",
+      "Microsoft",
+      "Google",
+      "Mozilla"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Choose the correct HTML element for the largest heading:",
+    answer: "h1",
+    options: [
+      "h1",
+      "h6",
+      "head",
+      "heading"
+    ]
+  },
+  {
+    numb: 8,
+    question: "What is the correct HTML element for inserting a line break?",
+    answer: "br",
+    options: [
+      "break",
+      "lb",
+      "br",
+      "BREAK"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Choose the correct HTML element to define important text",
+    answer: "strong",
+    options: [
+      "important",
+      "i",
+      "b",
+      "strong"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Choose the correct HTML element to define emphasized text",
+    answer: "em",
+    options: [
+      "em",
+      "i",
+      "italic",
+      "emphasized"
+    ]
+  },
+  {
+    numb: 11,
+    question: "Which character is used to indicate an end tag?",
+    answer: "/",
+    options: [
+      "<",
+      "^",
+      "*",
+      "/"
+    ]
+  },
+  // We can uncomment the below codes and make duplicate as more as we want to add question
+  // but remember we need to give the numb value serialize like 1,2,3,...
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
